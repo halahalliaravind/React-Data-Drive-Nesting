5aafd9a95299e5dd925f6b7f71da42160fdb3328
Aravind Kumar <aravindhalahalli.81@gmail.com>
2020-07-17 13:50:39 +0530

    Updates

---
+++ precache-manifest.1b00d91922dcb225dc25d6e2399417b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a53549309af47c6d1a53ad9f2168eb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a9d2c2e3e5b7c150169",
+    "url": "/static/css/main.b9256af2.chunk.css"
+  },
+  {
+    "revision": "742412c47aa6e8cf7747",
+    "url": "/static/js/2.87ff1d06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.87ff1d06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a9d2c2e3e5b7c150169",
+    "url": "/static/js/main.73628eae.chunk.js"
+  },
+  {
+    "revision": "f36c2ee8bb05b69437e0",
+    "url": "/static/js/runtime-main.2f6a320e.js"
+  }
+]);
